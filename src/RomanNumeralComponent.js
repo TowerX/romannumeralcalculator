53a5fe3358577de2f17c19b1a45a278fd924670c
TowerX <jptb86@gmail.com>
2019-10-02 11:23:20 -0500

    Create implementation

--- src/RomanNumeralComponent.js
+++ src/RomanNumeralComponent.js
@@ -6,7 +6,28 @@ export default function RomanNumeralComponent () {
 
 
     const addAndConvertToRomanNumerals = (ints) => {
-        /* Implement me! */
+        // calculate the sum of the elements
+        ints = ints.reduce((total, amount, index) => total + amount);
+        // store the resulting roman value
+        let result = "";
+        // array holding the decimals
+        let intsValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+        // array holding the roman numbers
+        let romanValue = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+        // iterate through the array and map the total to roman numbers (ints limit = 1000)
+        if (ints > 0 && ints < 1001) {
+            for (let i = 0; i < intsValue.length; i++) {
+                while (intsValue[i] <= ints) {
+                    result += romanValue[i];
+                    ints -= intsValue[i];
+                    console.log(result);
+                }
+            }
+            return result;
+        } else {
+            // default to nulla if ints === 0 is provided
+            ints = "Nulla";
+        }
         return ints;
     }
 
